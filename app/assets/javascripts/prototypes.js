076400576a57c96a4cbbef420b00e5c88cb1ebd7
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-03-15 11:02:38 +0900

    edit-thumnail-function-javascript-in-new-prototype
    WHAT
    新規投稿時に画像のサムネイル表示を行うjQuery追加
    WHY
    サムネイル画像を表示するため

--- app/assets/javascripts/prototypes.js
+++ app/assets/javascripts/prototypes.js
@@ -1,19 +1,39 @@
 // 参考資料
 // https://www.cresco.co.jp/blog/entry/710/
 $(document).on('turbolinks:load',function(){
-  // コンソールから確認したid
+  // コンソールから確認したid。0がついたidはメイン画像
   // prototype_captured_images_attributes_0_content
-  $("#prototype_captured_images_attributes_0_content").on('change',function(){
+  // 以降のサブは連番であるので前方一致と後方一致で指定する
+  // 参考資料
+  // https://teratail.com/questions/36145
+  $('[id^="prototype_captured_images_attributes_"][id$="_content"]').on('change',function(){
     if (this.files.length > 0){
       // ファイル情報取得
       var file = this.files[0];
+      // 対象のオブジェクトを変数に保存
+      // onloadのfunctionの部分で直接指定できないため
+      var target = $(this)
+      // 背景画像消去するためにcss指定→ひとまず使わない方向で
+      // var target_css = target.parent().attr('class');
+      console.log('.' + target);
       // ファイル読み込み？
       var reader = new FileReader();
+      // ファイルのURLを取得
       reader.readAsDataURL(file);
-      // onloadについてよくわかってません
+      // onload：読み込みが正常に完了したときに発火
       reader.onload = function(){
-        // app/views/prototypes/new.html.hamlの15行目のimgにsrcでファイルパスを指定する
-        $("#main_image_uploader").children('img').attr('src',reader.result);
+        target.prev('img').attr('src',reader.result);
+        // サムネイルを縦横比揃えて表示するようにcssを設定
+        target.prev('img').css({
+          'width':'auto',
+          'height':'auto',
+          'max-width':'100%',
+          'max-height':'100%'
+        });
+        // 初期の背景画像を消す→ひとまず使わない方向で
+        // $('.' + target_css).css({
+        //   "backgroundImage": 'none'
+        // });
       }
     }
   });
