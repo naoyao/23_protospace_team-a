9ad393c10770c781420d580029afa71f24b7f969
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-03-23 15:56:54 +0900

    Revert "little-change"
    This reverts commit 108a3e2c30e90f0b0f670059e40869fa2f7b2b9f.

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,14 +1,14 @@
 $(function(){
   function buildHTML(comment){
-    var html = '<div class="media">' +
-                '  <div class="media-left">' +
-                '    <img src="${comment.user_name}" style="width: 64px; height: 64px;">' +
-                '  </div>' +
-                '  <div class="media-body">' +
-                '    <h4 class="media-heading" id="top-aligned-media">${comment.user_name}</h4>' +
-                '    <p>${comment.text}</p>' +
-                '  </div>' +
-                '</div>';
+    var html = `<div class="media">
+                  <div class='media-left'>
+                    <img src='${comment.user_name}' style='width: 64px; height: 64px;'>
+                  </div>
+                  <div class='media-body'>
+                    <h4 class='media-heading' id='top-aligned-media'>${comment.user_name}</h4>
+                    <p>${comment.text}</p>
+                  </div>
+                </div>`
     return html;
   }
   $('#new_comment').on('submit', function(e){
